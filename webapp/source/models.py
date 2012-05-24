ec86bf37b689fc5addc43e1c3d9083871011a584
alexb <alex.bruckner@semantico.com>
2012-05-24 21:09:27 +0100

    added source model

--- webapp/source/models.py
+++ webapp/source/models.py
@@ -1,3 +1,22 @@
+from django import forms
 from django.db import models
+from django.contrib import admin
 
 # Create your models here.
+
+class Source(models.Model):
+    user = models.CharField(max_length=50)
+    source = models.CharField(max_length=1000)
+
+    def __unicode__(self):
+        return self.name
+
+class SourceForm(forms.ModelForm):
+    source = forms.CharField( widget=forms.Textarea )
+    class Meta:
+        model = Source
+
+class SourceAdmin(admin.ModelAdmin):
+    form = SourceForm
+
+admin.site.register(Source, SourceAdmin)
